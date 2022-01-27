cf9d6653405702e91bfe132f5f255c1b7d90c731
Kosov Ruslan <korus86@mail.ru>
2022-01-27 19:56:53 +0300

    end of DZ

---
+++ lesson5-6-DZForms/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Questions</title>
+</head>
+<body>
+    <form action="source.php" method="post">
+        <p>Вопрос 1</p>
+        <input type="radio" name="rQuest1" value="Answer1">
+        <label for="rQuest1">Answer1</label>
+        <input type="radio" name="rQuest1" value="Answer2">
+        <label for="rQuest1">Answer2</label>
+        <input type="radio" name="rQuest1" value="Answer3">
+        <label for="rQuest1">Answer3</label>
+        <input type="radio" name="rQuest1" value="Answer4">
+        <label for="rQuest1">Answer4</label>
+        <p>Вопрос 2</p>
+        <input type="radio" name="rQuest2" value="Answer1">
+        <label for="rQuest2">Answer1</label>
+        <input type="radio" name="rQuest2" value="Answer2">
+        <label for="rQuest2">Answer2</label>
+        <input type="radio" name="rQuest2" value="Answer3">
+        <label for="rQuest2">Answer3</label>
+        <input type="radio" name="rQuest2" value="Answer4">
+        <label for="rQuest2">Answer4</label>
+        <p>Вопрос 3</p>
+        <input type="radio" name="rQuest3" value="Answer1">
+        <label for="rQuest3">Answer1</label>
+        <input type="radio" name="rQuest3" value="Answer2">
+        <label for="rQuest3">Answer2</label>
+        <input type="radio" name="rQuest3" value="Answer3">
+        <label for="rQuest3">Answer3</label>
+        <input type="radio" name="rQuest3" value="Answer4">
+        <label for="rQuest3">Answer4</label>
+        <p>Вопрос 4</p>
+        <input type="radio" name="rQuest4" value="Answer1">
+        <label for="rQuest4">Answer1</label>
+        <input type="radio" name="rQuest4" value="Answer2">
+        <label for="rQuest4">Answer2</label>
+        <input type="radio" name="rQuest4" value="Answer3">
+        <label for="rQuest4">Answer3</label>
+        <input type="radio" name="rQuest4" value="Answer4">
+        <label for="rQuest4">Answer4</label>
+        <p>Вопрос 5</p>
+        <input type="radio" name="rQuest5" value="Answer1">
+        <label for="rQuest5">Answer1</label>
+        <input type="radio" name="rQuest5" value="Answer2">
+        <label for="rQuest5">Answer2</label>
+        <input type="radio" name="rQuest5" value="Answer3">
+        <label for="rQuest5">Answer3</label>
+        <input type="radio" name="rQuest5" value="Answer4">
+        <label for="rQuest5">Answer4</label>
+        <br>
+        <button type="submit" name="nNext" value="nNext1">Next</button>
+    </form>
+</body>
+</html>
---
+++ lesson5-6-DZForms/source.php
@@ -0,0 +1,90 @@
+<?php
+
+if ($_POST['nNext'] === 'nNext1') {
+    $countOfRightAnswers = 0;
+
+    if ($_POST['rQuest1'] === 'Answer1') {
+        $countOfRightAnswers ++;
+    }
+    if ($_POST['rQuest2'] === 'Answer3') {
+        $countOfRightAnswers ++;
+    }
+    if ($_POST['rQuest3'] === 'Answer4') {
+        $countOfRightAnswers ++;
+    }
+    if ($_POST['rQuest4'] === 'Answer1') {
+        $countOfRightAnswers ++;
+    }
+    if ($_POST['rQuest5'] === 'Answer2') {
+        $countOfRightAnswers ++;
+    }
+    header('Refresh:0, url=page2.php?right_answers=' . $countOfRightAnswers);
+}
+elseif ($_POST['nNext'] === 'nNext2') {
+    $countOfRightAnswersPage1 = $_POST['countOfRightAnswersPage1'];
+    $countOfRightAnswersPage2 = 0;
+
+    if (count($_POST['rQuest1']) == 1) {
+        $rQuest1 = $_POST['rQuest1'];
+        if ($rQuest1[0] === 'Answer2') {
+            $countOfRightAnswersPage2 = $countOfRightAnswersPage2+3;
+        }
+    }
+    if (count($_POST['rQuest2']) == 2) {
+        $rQuest2 = $_POST['rQuest2'];
+        if (
+            ($rQuest2[0] === 'Answer1' || $rQuest2[0] === 'Answer3') &&
+            ($rQuest2[1] === 'Answer1' || $rQuest2[1] === 'Answer3')
+        ) {
+            $countOfRightAnswersPage2 = $countOfRightAnswersPage2+3;
+        }
+    }
+    if (count($_POST['rQuest3']) == 3) {
+        $rQuest3 = $_POST['rQuest3'];
+        if (
+            ($rQuest3[0] === 'Answer2' || $rQuest3[0] === 'Answer3' || $rQuest3[0] === 'Answer4') &&
+            ($rQuest3[1] === 'Answer2' || $rQuest3[1] === 'Answer3' || $rQuest3[1] === 'Answer4') &&
+            ($rQuest3[2] === 'Answer2' || $rQuest3[2] === 'Answer3' || $rQuest3[2] === 'Answer4')
+        ) {
+            $countOfRightAnswersPage2 = $countOfRightAnswersPage2+3;
+        }
+    }
+    if (count($_POST['rQuest4']) == 1) {
+        $rQuest4 = $_POST['rQuest4'];
+        if ($rQuest4[0] === 'Answer2') {
+            $countOfRightAnswersPage2 = $countOfRightAnswersPage2+3;
+        }
+    }
+    if (count($_POST['rQuest5']) == 1) {
+        $rQuest5 = $_POST['rQuest5'];
+        if ($rQuest5[0] === 'Answer1') {
+            $countOfRightAnswersPage2 = $countOfRightAnswersPage2+3;
+        }
+    }
+    $result = $countOfRightAnswersPage1 + $countOfRightAnswersPage2;
+    header('Refresh:0, url=page3.php?right_answers=' . $result);    
+}
+elseif ($_POST['nNext'] === 'Finish') {
+    $result = $_POST['countOfRightAnswersPage2'];
+    $countOfRightAnswersPage3 = 0;
+    if(mb_strtolower($_POST['rQuest1']) === 'moscow') {
+    $countOfRightAnswersPage3 += 5;
+    }
+    if(mb_strtolower($_POST['rQuest2']) === 'kiev') {
+    $countOfRightAnswersPage3 += 5;
+    }
+    if(mb_strtolower($_POST['rQuest3']) === 'moscow') {
+    $countOfRightAnswersPage3 += 5;
+    }
+    if(mb_strtolower($_POST['rQuest4']) === 'moscow') {
+    $countOfRightAnswersPage3 += 5;
+    }
+    if(mb_strtolower($_POST['rQuest5']) === 'moscow') {
+    $countOfRightAnswersPage3 += 5;
+    }
+    
+    $result +=  $countOfRightAnswersPage3;
+
+    echo '<p>Your score is: '. $result . '</p>';
+}
+
